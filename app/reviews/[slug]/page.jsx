1a261423bb3b723f793792a51e4008e2623ccd55
TaimoorZahid94 <taimoor@borialas.com>
2023-11-17 16:38:51 +0500

    Added newest review first by order of review added

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,12 +1,18 @@
 import Heading from '@/components/Heading';
-import { getReview } from '@/lib/reviews';
+import { getReview, getSlugs } from '@/lib/reviews';
+
+export async function generateStaticParams() {
+  const slugs = await getSlugs();
+  return slugs.map((slug) => ({ slug }));
+}
 
 export default async function ReviewPage({ params: { slug } }) {
-    const review = await getReview(slug);
+  const review = await getReview(slug);
+  console.log('[ReviewPage] rendering', slug);
   return (
     <>
       <Heading>{review.title}</Heading>
-      <p className='italic pb-2'>{review.date}</p>
+      <p className="italic pb-2">{review.date}</p>
       <img src={review.image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
---
+++ content/reviews/hellblade.md
@@ -0,0 +1,7 @@
+---
+title: "Hell Blade"
+date: "2023-05-06"
+image: "/images/hellblade.jpg"
+---
+
+This is the review for the __Hellblade__.
