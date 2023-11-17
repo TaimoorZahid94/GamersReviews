da12ef9d75c3888693d90daee1ac784c3693c7f2
TaimoorZahid94 <taimoor@borialas.com>
2023-11-17 12:53:07 +0500

    Added Tailwind Typography and reviews data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import NavBar from '../components/NavBar';
 import { exo2, orbitron } from './fonts';
 import './globals.css';
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${exo2.variable} ${orbitron.variable}`}>
@@ -12,7 +11,7 @@ export default function RootLayout({ children }) {
         <main className="grow py-3">
           {children}
         </main>
-        <footer className="border-t py-3 text-center text-xs">
+        <footer className="border-t py-3 text-center text-slate-500 text-xs">
           Game data and images courtesy of{' '}
           <a href="https://rawg.io/" target="_blank"
             className="text-orange-800 hover:underline">
--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,14 +1,18 @@
-import Heading from "@/components/Heading";
-
-export default function StardewValleyPage() {
-    return (
-        <>
-            <Heading>Stardew Valley</Heading>
-            <img src="/images/stardew-valley.jpg" alt="hollow knight img" width="640" height="360" className="mb-2 rounded"></img>
-            <p>
-                This will be the review for Stardew Valley.
-            </p>
-        </>
-
-    );
+import { readFile } from 'node:fs/promises';
+import { marked } from 'marked';
+import Heading from '@/components/Heading';
+export default async function StardewValleyPage() {
+  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
+  const html = marked(text);
+  return (
+    <>
+      <Heading>Stardew Valley</Heading>
+      <img src="/images/stardew-valley.jpg" alt=""
+        width="640" height="360" className="mb-2 rounded"
+      />
+      <article dangerouslySetInnerHTML={{ __html: html }}
+        className="max-w-screen-sm prose prose-slate"
+      />
+    </>
+  );
 }
