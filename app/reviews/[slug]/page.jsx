e59c28d30e2bcafdf58adbefa0b1042fe568ddab
TaimoorZahid94 <taimoor@borialas.com>
2023-11-20 16:47:20 +0500

    Added subtitles and lazy loader

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -4,7 +4,7 @@ import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
 export async function generateStaticParams() {
   const slugs = await getSlugs();
-  console.log('[ReviewPage] generateStaticParams:', slugs);
+  // console.log('[ReviewPage] generateStaticParams:', slugs);
   return slugs.map((slug) => ({ slug }));
 }
 export async function generateMetadata({ params: { slug } }) {
@@ -19,11 +19,14 @@ export default async function ReviewPage({ params: { slug } }) {
   return (
     <>
       <Heading>{review.title}</Heading>
+      <p className="font-semibold pb-3">
+        {review.subtitle}
+      </p>
       <div className="flex gap-3 items-baseline">
         <p className="italic pb-2">{review.date}</p>
         <ShareLinkButton />
       </div>
-      <Image src={review.image} alt=""
+      <Image src={review.image} alt="" priority
         width="640" height="360" className="mb-2 rounded"
       />
       <article dangerouslySetInnerHTML={{ __html: review.body }}
--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
@@ -6,17 +7,17 @@ export const metadata = {
 };
 
 export default async function ReviewsPage() {
-  const reviews = await getReviews();
+  const reviews = await getReviews(6);
   // console.log('[ReviewsPage] reviews:', reviews);
   return (
     <>
       <Heading>Reviews</Heading>
       <ul className="flex flex-row flex-wrap gap-3">
-        {reviews.map((review) => (
+        {reviews.map((review, index) => (
           <li key={review.slug}
             className="bg-white border rounded shadow w-80 hover:shadow-xl">
             <Link href={`/reviews/${review.slug}`}>
-              <img src={review.image} alt=""
+              <Image src={review.image} alt="" priority={index === 0}
                 width="320" height="180" className="rounded-t"
               />
               <h2 className="font-orbitron font-semibold py-1 text-center">
