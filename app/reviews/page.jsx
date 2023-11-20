128bd67279e98a658f275e0fda1a86209726f07b
TaimoorZahid94 <taimoor@borialas.com>
2023-11-20 15:30:30 +0500

    fetched reviews api from strapi code refactoring

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import Heading from '@/components/Heading';
 import { getReviews } from '@/lib/reviews';
-
 export const metadata = {
   title: 'Reviews',
 };
 
 export default async function ReviewsPage() {
   const reviews = await getReviews();
+  // console.log('[ReviewsPage] reviews:', reviews);
   return (
     <>
       <Heading>Reviews</Heading>
