0591362ad17c7f5b41cd0c9ae1d22e9c5357734b
TaimoorZahid94 <taimoor@borialas.com>
2023-11-20 11:32:13 +0500

    Added Link Favicon

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { LinkIcon } from '@heroicons/react/20/solid';
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -11,8 +11,9 @@ export default function ShareLinkButton() {
     };
     return (
         <button onClick={handleClick}
-            className="border px-2 py-1 rounded text-slate-500 text-sm
+            className="border flex gap-1 items-center px-2 py-1 rounded text-slate-500 text-sm
                  hover:bg-orange-100 hover:text-slate-700">
+                    <LinkIcon className='h-4 w-4' />
             {clicked ? "Link Copied" : "Share link"}
         </button>
     );
