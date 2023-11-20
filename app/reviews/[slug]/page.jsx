79aa9f82943b6793c6dd663f18d2b373c0ee2aab
TaimoorZahid94 <taimoor@borialas.com>
2023-11-20 15:53:58 +0500

    Image Optimization

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,20 +1,18 @@
+import Image from 'next/image';
 import Heading from '@/components/Heading';
 import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
-
 export async function generateStaticParams() {
   const slugs = await getSlugs();
   console.log('[ReviewPage] generateStaticParams:', slugs);
   return slugs.map((slug) => ({ slug }));
 }
-
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug);
   return {
     title: review.title,
   };
 }
-
 export default async function ReviewPage({ params: { slug } }) {
   const review = await getReview(slug);
   // console.log('[ReviewPage] review', review);
@@ -25,7 +23,7 @@ export default async function ReviewPage({ params: { slug } }) {
         <p className="italic pb-2">{review.date}</p>
         <ShareLinkButton />
       </div>
-      <img src={review.image} alt=""
+      <Image src={review.image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
       <article dangerouslySetInnerHTML={{ __html: review.body }}
