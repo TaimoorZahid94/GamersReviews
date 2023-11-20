266535d0eb718f9e37a1cf82058f19b066432320
TaimoorZahid94 <taimoor@borialas.com>
2023-11-20 11:10:32 +0500

    Added ShareLink button

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,25 +1,27 @@
 import Heading from '@/components/Heading';
+import ShareLinkButton from '@/components/ShareLinkButton';
 import { getReview, getSlugs } from '@/lib/reviews';
 
 export async function generateStaticParams() {
   const slugs = await getSlugs();
   return slugs.map((slug) => ({ slug }));
 }
-
 export async function generateMetadata({ params: { slug } }) {
   const review = await getReview(slug);
   return {
     title: review.title,
-  }
+  };
 }
-
 export default async function ReviewPage({ params: { slug } }) {
   const review = await getReview(slug);
   console.log('[ReviewPage] rendering', slug);
   return (
     <>
       <Heading>{review.title}</Heading>
-      <p className="italic pb-2">{review.date}</p>
+      <div className="flex gap-3 items-baseline">
+        <p className="italic pb-2">{review.date}</p>
+        <ShareLinkButton />
+      </div>
       <img src={review.image} alt=""
         width="640" height="360" className="mb-2 rounded"
       />
